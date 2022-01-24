8942b890432f067908bf2a89ceb25606482ff7a1
pattWill <pattywilliams20@gmail.com>
2022-01-24 20:13:13 -0300

    new project mercado liebre with T27

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const publicPath = path.resolve(__dirname, './public')
+
+app.use( express.static(publicPath))
+
+
+app.listen(3030, ()  => console.log('Levantando un servidor con Express'));
