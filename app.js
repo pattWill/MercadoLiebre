96432aca08a417322434a057aa4fa6a2b739376f
pattWill <pattywilliams20@gmail.com>
2022-01-24 21:05:47 -0300

    Nodemon install, and completing the mockup with the logo and the menu

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const publicPath = path.resolve(__dirname, './public')
 app.use( express.static(publicPath))
 
 
-app.listen(3030, ()  => console.log('Levantando un servidor con Express'));
+app.listen(3030, ()  => console.log('Mercado Liebre está corriendo!!!'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
