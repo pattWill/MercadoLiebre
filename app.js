bc0b2c359e12698f980409cae8fc0c5251f2f8e6
pattWill <pattywilliams20@gmail.com>
2022-02-18 15:20:36 -0300

    delete node modules

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const publicPath = path.resolve(__dirname, './public')
 app.use( express.static(publicPath))
 
 
-app.listen(3030, ()  => console.log('Mercado Liebre está corriendo!!!'));
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log('Servidor funcionando');
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
